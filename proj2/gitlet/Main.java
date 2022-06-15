0877641e1842ed8bf9180f63b67b7ab38c2405ef
Badwoman <lianyukang@foxmail.com>
2022-06-16 01:03:11 +0800

    Finish some args validation

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,24 +1,43 @@
 package gitlet;
 
-/** Driver class for Gitlet, a subset of the Git version-control system.
- *  @author TODO
+/**
+ * Driver class for Gitlet, a subset of the Git version-control system.
+ * <p>
+ * * @author abmdocrt
  */
 public class Main {
 
-    /** Usage: java gitlet.Main ARGS, where ARGS contains
-     *  <COMMAND> <OPERAND1> <OPERAND2> ... 
+    /**
+     * Usage: java gitlet.Main ARGS, where ARGS contains
+     * <COMMAND> <OPERAND1> <OPERAND2> ...
      */
     public static void main(String[] args) {
         // TODO: what if args is empty?
+        if (args.length == 0) {
+            System.out.println("Please enter a command.");
+            System.exit(0);
+        }
         String firstArg = args[0];
-        switch(firstArg) {
+        switch (firstArg) {
             case "init":
                 // TODO: handle the `init` command
+                validArgs(args, 1);
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command
+                validArgs(args, 2);
                 break;
             // TODO: FILL THE REST IN
+            default:
+                System.out.println("No command with that name exists.");
+                System.exit(0);
+        }
+    }
+
+    private static void validArgs(String[] args, int num) {
+        if (args.length != num) {
+            System.out.println("Incorrect operands.");
+            System.exit(0);
         }
     }
 }
