956016a4e5fa06e176f0c54e189f4199a8b8ee6f
Badwoman <lianyukang@foxmail.com>
2022-06-18 01:47:19 +0800

    Finish Gitlet Commit Function

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -2,7 +2,7 @@ package gitlet;
 
 import java.io.File;
 import java.nio.file.Paths;
-import java.util.Date;
+import java.util.*;
 
 import static gitlet.Utils.*;
 
@@ -50,7 +50,7 @@ public class Repository {
     public static final File REFS_DIR = join(GITLET_DIR, "refs");
     public static final File HEADS_DIR = join(REFS_DIR, "heads");
     public static final File HEAD_FILE = join(GITLET_DIR, "HEAD");
-    public static final File STAGE = join(GITLET_DIR, "stage");
+    public static final File ADDSTAGE_FILE = join(GITLET_DIR, "add_stage");
 
     public static Commit currCommmit;
 
@@ -83,7 +83,7 @@ public class Repository {
     }
 
     private static void initHEAD() {
-        writeContents(HEAD_FILE, "ref: refs/heads/master");
+        writeContents(HEAD_FILE, "master");
     }
 
     private static void initCommit() {
@@ -105,13 +105,26 @@ public class Repository {
             System.exit(0);
         }
         Blob blob = new Blob(fileName);
+        storeBlob(blob);
+    }
+
+    private static void storeBlob(Blob blob) {
+        addStage = readStage();
         if (addStage.isNewBlob(blob)) {
             blob.save();
             if (addStage.isFilePathExists(blob.getPath())) {
                 addStage.delete(blob);
             }
             addStage.add(blob);
+            addStage.save();
         }
+    }
+
+    private static Stage readStage() {
+        if (!ADDSTAGE_FILE.exists()) {
+            return new Stage();
+        }
+        return readObject(ADDSTAGE_FILE, Stage.class);
     }
 
     private static File getFileFromCWD(String file) {
@@ -120,8 +133,73 @@ public class Repository {
                 : join(CWD, file);
     }
 
-    /* * add command funtion */
-    public static void commit(String message){
+    /* * commit command funtion */
+    public static void commit(String message) {
+        if (message.equals("")) {
+            System.out.println("Please enter a commit message.");
+            System.exit(0);
+        }
+        Commit newCommit = newCommit(message);
+        newCommit.save();
+        addStage.clear();
+        addStage.save();
+        saveHeads(newCommit);
+    }
 
+    private static Commit newCommit(String message) {
+        Map<String, String> blobMap = findBlobMap();
+        checkIfNewCommit(blobMap);
+        List<String> parents = findParents();
+        return new Commit(message, blobMap, parents);
+    }
+
+    private static Map<String, String> findBlobMap() {
+        Map<String, String> blobMap = new HashMap<>();
+        addStage = readStage();
+        List<Blob> blobList = addStage.getBlobList();
+        for (Blob b : blobList) {
+            blobMap.put(b.getPath(), b.getBlobID());
+        }
+        return blobMap;
+    }
+
+    private static void checkIfNewCommit(Map<String, String> blobMap) {
+        if (blobMap.isEmpty()) {
+            System.out.println("No changes added to the commit.");
+            System.exit(0);
+        }
+    }
+
+    private static List<String> findParents() {
+        List<String> parents = new ArrayList<>();
+        currCommmit = readCurrCommmit();
+        parents.add(currCommmit.getID());
+        return parents;
+    }
+
+    private static Commit readCurrCommmit() {
+        String currCommmitID = readCurrCommmitID();
+        String dirName = currCommmitID.substring(0, 2);
+        String fileName = currCommmitID.substring(2);
+        File CURR_COMMIT_DIR = join(OBJECT_DIR, dirName);
+        File CURR_COMMIT_FILE = join(CURR_COMMIT_DIR, fileName);
+        return readObject(CURR_COMMIT_FILE, Commit.class);
+    }
+
+    private static String readCurrCommmitID() {
+        String currBranch = readCurrBranch();
+        File HEADS_FILE = join(HEADS_DIR, currBranch);
+        return readContentsAsString(HEADS_FILE);
+    }
+
+    private static void saveHeads(Commit newCommit) {
+        currCommmit = newCommit;
+        String currBranch = readCurrBranch();
+        File HEADS_FILE = join(HEADS_DIR, currBranch);
+        writeContents(HEADS_FILE, currCommmit.getID());
+    }
+
+    private static String readCurrBranch() {
+        return readContentsAsString(HEAD_FILE);
     }
 }
--- proj2/gitlet/Stage.java
+++ proj2/gitlet/Stage.java
@@ -1,15 +1,24 @@
 package gitlet;
 
+import java.io.File;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Stage {
+import static gitlet.Repository.OBJECT_DIR;
+import static gitlet.Utils.*;
 
-    private static List<String> blobList = new ArrayList<>();
-    private static List<String> filePath = new ArrayList<>();
+public class Stage implements Serializable {
+
+    private List<String> blobIDList = new ArrayList<>();
+    private List<String> filePath = new ArrayList<>();
+
+    public List<String> getBIL(){
+        return blobIDList;
+    }
 
     public boolean isNewBlob(Blob blob) {
-        if (!blobList.contains(blob.getBlobID())) {
+        if (!blobIDList.contains(blob.getBlobID())) {
             return true;
         }
         return false;
@@ -23,16 +32,43 @@ public class Stage {
     }
 
     public void delete(Blob blob) {
-        blobList.remove(blob.getBlobID());
+        blobIDList.remove(blob.getBlobID());
         filePath.remove(blob.getPath());
     }
 
     public void add(Blob blob) {
-        if (!blobList.contains(blob.getBlobID())) {
-            blobList.add(blob.getBlobID());
+        if (!blobIDList.contains(blob.getBlobID())) {
+            blobIDList.add(blob.getBlobID());
         }
         if (!filePath.contains(blob.getPath())) {
             filePath.add(blob.getPath());
         }
+    }
+
+    public void save() {
+        writeObject(Repository.ADDSTAGE_FILE, this);
+    }
+
+    public void clear() {
+        blobIDList.clear();
+        filePath.clear();
+    }
+
+    public List<Blob> getBlobList() {
+        Blob blob;
+        List<Blob> blobList = new ArrayList<>();
+        for (String id : blobIDList) {
+            blob = getBlob(id);
+            blobList.add(blob);
+        }
+        return blobList;
+    }
+
+    private Blob getBlob(String id) {
+        String dirName = id.substring(0, 2);
+        String fileName = id.substring(2);
+        File BLOB_DIR = join(OBJECT_DIR, dirName);
+        File BLOB_FILE = join(BLOB_DIR, fileName);
+        return readObject(BLOB_FILE, Blob.class);
     }
 }
